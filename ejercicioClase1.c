41520f76f245a634253afcbd1e19b3fe1e8c0bd5
shiro-saber <shiro-saber@debian.debian>
2016-01-12 20:51:39 -0600

    Ejercicio en clase

--- ejercicioClase1.c
+++ ejercicioClase1.c
@@ -1,7 +1,7 @@
 /* -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.
 * File Name : main.c
 * Creation Date : 11-01-2016
-* Last Modified : Tue 12 Jan 2016 11:01:22 AM CST
+* Last Modified : Tue 12 Jan 2016 08:51:09 PM CST
 * Created By : shiro-saber
 
 KNOW LEARN        .==.
@@ -19,6 +19,8 @@ _._._._._._._._._._._._._._._._._._._._._.*/
 #include <stdio.h>
 #include <stdlib.h>
 
+#define kuz 5
+
 int i=0;
 
 typedef struct
@@ -31,14 +33,14 @@ typedef struct
 void agregarPersona(Persona *p, int a) //para agregar personas al arreglo
 {
   (p+a)->nombre = malloc(sizeof(char)*10);
-  printf("Dame el nombre: ");
+  printf("Dame el nombre: \n");
   scanf("%s",(p+a)->nombre);
 
   (p+a)->apellido = malloc(sizeof(char)*10);
-  printf("Dame el apellido: ");
+  printf("Dame el apellido: \n");
   scanf("%s",(p+a)->apellido);
 
-  printf("Dame la edad: ");
+  printf("Dame la edad: \n");
   scanf("%d",&(p+a)->edad);
 }
 
@@ -67,41 +69,61 @@ int main()
 {
   Persona *p;
   int tamP = 0;//tamaño del arreglo dinámico
-
-  /*Aquí debería haber un menu*/
+  int sharmutta = -1; //la opcion de cases
 
   //sección del arreglo dinámico
   p = (Persona*)malloc(1*sizeof(Persona));
-  p = (Persona*)realloc(p, (tamP+1) * sizeof(Persona));
 
-  agregarPersona(p, tamP);
-  tamP++;
-
-  /*sección de promedio de edad, entre edades y el más viejo */
-  //promedio de edad
-  int promedio;
-
-  for(i = 0; i < tamP; ++i)
-    promedio = (p+i)->edad;
-
-  promedio = promedio/tamP;
-  printf("El promedio de edad es %s", promedio);
-
-  //entre edades
-
-
-  //el más viejo
-  Persona *res;
-
-  for (i = 0; i < tamP; ++i)
+  while(sharmutta != 5)
   {
-    if(i+1 != tamP && (p+i)->edad < (p+i+1)->edad)
-      res = p;
-  }
-  printf("La persona mas vieja es %s %s %d", res->nombre, res->apellido, res->edad);
-  free(res);
+    printf("Menu\n   1. Agregar Persona\n   2. Promedio\n   3. Entre edades\n   4. El mas viejo\n   5. Salir\n");
+    scanf("%d", &sharmutta);
+    
+    if(sharmutta == 1)
+    {
+      p = (Persona*)realloc(p, (tamP+1) * sizeof(Persona));
+      agregarPersona(p, tamP);
+      tamP++;
+      printf("%s %s %d agregado correctamente\n", p->nombre, p->apellido, p->edad);
+    }
 
-  /*sección de imprimir*/
+    /*sección de promedio de edad, entre edades y el más viejo */
+    else if(sharmutta == 2)
+    {
+      //promedio de edad
+      int promedio;
+
+      for(i = 0; i < tamP; ++i)
+        promedio += (p+i)->edad;
+
+      promedio = promedio/tamP;
+      printf("El promedio de edad es %s\n", promedio);
+    
+    }
+    else if(sharmutta == 3)
+    {
+      //entre edades
+    }
+    else if(sharmutta == 4)
+    {
+      //el más viejo
+      Persona *res;
+
+      for (i = 0; i < tamP; ++i)
+      {
+        if(i+1 != tamP && (p+i)->edad < (p+i+1)->edad && tamP != 1)
+          res = p;
+      }
+      printf("La persona mas vieja es %s %s %d\n", res->nombre, res->apellido, res->edad);
+      free(res);
+    }
+    else if(sharmutta == 5)
+    {
+      printf("BYE!\n");
+    }
+    else
+      printf("No es opcion ¬¬\n");
+  }
 
   limpiar(p,tamP);//no memory leaks
   return 0;
