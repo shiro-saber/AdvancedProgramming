3b36abc3e9626fd39a3749c28fc7b74786afb2b5
shiro-saber <shiro.archerl@gmail.com>
2016-01-28 10:44:47 -0600

    :(

--- examen_rapido_2/ejercicio.c
+++ examen_rapido_2/ejercicio.c
@@ -1,7 +1,7 @@
 /* -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.
 * File Name : ejercicio.c
 * Creation Date : 28-01-2016
-* Last Modified : Thu 28 Jan 2016 10:21:14 AM CST
+* Last Modified : Thu 28 Jan 2016 10:44:36 AM CST
 * Created By : shiro-saber
 
 KNOW LEARN        .==.
@@ -18,6 +18,7 @@ _._._._._._._._._._._._._._._._._._._._._.*/
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /*Make a forward iterator, a backward iterator and a bidirectional iterator*/
 
@@ -25,26 +26,84 @@ typedef struct
 {
   char *titulo;
   int paginas;
+  int status;
 }Libro;
 
 typedef struct
 {
-  char *name;
-  char direction;
-  Libro *begin;
-  Libro *next;
-  Libro *previous;
-  Libro *end;
+  int position;
+  char *direction;
+  char *actual_direction;//this is for the bi directional
 }Iterator;
 
 void define_iterator(Iterator *i, int kuz)
 {
+  int option;
+  
+  printf("Your iterator it's going to be:\n1. Forward\n2. Backward\n 3. Bidirectional\n");
+  scanf("%d", &option);
 
+  if(option == 1)
+    i->direction = ("forward");
+  else if(option == 2)
+    i->direction = ("backward");
+  else if(option == 3)
+    i->direction = ("bidirectional");
+  else
+    printf("This option doesn't exists\n");
+}
+
+Libro *next(Libro *l, Iterator *i, int user_begin)
+{
+  if(i->direction == "forward")
+    i->position = user_begin+1;
+  else if(i->direction == "bidirectional")
+    i->position = user_begin+1;
+
+
+  return (l+i->position);
+}
+
+Libro *prev(Libro *l, Iterator *i, int user_begin)
+{
+  if(i->direction == "bidirectional")
+    i->position = user_begin-1;
+  else if(i->direction == "backward")
+    i->position = user_begin-1;
+
+
+  return (l+i->position);
+}
+
+Libro *begin(Libro *l, Iterator *i, int kuz)//donde kuz será el tamaño de los  libros
+{
+  if (i->direction == ("forward") || i->direction == ("bidirectional"))
+    i->position = 0;
+  else if(i->direction == ("backward"))
+      if((l+kuz-1)->status == 1)
+       (i->position) = kuz;
+      else
+        kuz--;
+  
+  return (l+i->position);
+}
+
+Libro *end(Libro *l, Iterator *i, int kuz)
+{
+  if (i->direction == ("forward") || i->direction == ("bidirectional"))
+    i->position = 0;
+  else if(i->direction == ("backward"))
+      if((l+kuz-1)->status == 1)
+       (i->position) = kuz;
+      else
+        kuz--;
+  
+  return (l+i->position);
 }
 
 int main()
 {
-
+  
 
   return 0;
 }
