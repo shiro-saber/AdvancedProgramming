3ee86f158a25b261c290e2fea1d458507bf210c2
shiro-saber <shiro.archerl@gmail.com>
2016-02-07 23:49:05 -0600

    Preparing process quick exam

---
+++ Quick_Exams/process_exam.c
@@ -0,0 +1,28 @@
+/* -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.
+* File Name : process_exam.c
+* Creation Date : 07-02-2016
+* Last Modified : Sun 07 Feb 2016 11:42:46 PM CST
+* Created By : shiro-saber
+
+KNOW LEARN        .==.
+C LANGUAGE       ()''()-.
+      .---.       ;--; /
+    .'_:___". _..'.  __'.
+    |__ --==|'-''' \'...;
+   [  ]  :[|       |---\
+    |__| I=[|     .'    '.
+    / / ____|     :       '._
+   |-/.____.'      | :       :
+  /___\ /___\      '-'._----'
+_._._._._._._._._._._._._._._._._._._._._.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+  
+
+  return 0;
+}
+
