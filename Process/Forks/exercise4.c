0561a2d2a547824b7acc52965d7840fc655859f5
shiro-saber <shiro.archerl@gmail.com>
2016-02-09 14:45:39 -0600

    Tarea 2

--- Process/Forks/exercise4.c
+++ Process/Forks/exercise4.c
@@ -1,7 +1,7 @@
 /* -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.
 * File Name : exercise4.c
 * Creation Date : 04-02-2016
-* Last Modified : Mon 08 Feb 2016 11:18:58 AM CST
+* Last Modified : Tue 09 Feb 2016 02:45:29 PM CST
 * Created By : shiro-saber
 
 KNOW LEARN        .==.
@@ -19,49 +19,64 @@ _._._._._._._._._._._._._._._._._._._._._.*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <wait.h>
+#include <sys/wait.h>
 #include <sys/types.h>
+
+static void wait_for_kids(void)
+{
+    int corpse;
+    int status;
+    while ((corpse = wait(&status)) > 0)
+        /* printf("Child %d exited with status 0x%.4X\n", corpse, status); */
+        ;
+}
 
 int main(int argc, char *argv[])
 {
-  pid_t child;
-  int i, j, k;
-  int lvl = atoi(argv[1]);
-  int proc = atoi(argv[2]);
+    pid_t child;
+    int j, k;
 
-  if(argc != 3)
-  {
-    printf("Syntax: ./pstree <number of levels> <number of process in each level>\n");
-    
-    return -1;
-  }
-
-  printf("%s %d %d\n", argv[0], lvl, proc);
-
-  for (k = 0; k < lvl; k++)
-  {
-    for(j = 0; j < proc; j++)
+    if (argc != 3)
     {
-      child = fork();
-
-      if(child < 0)
-      {
-        printf("Error creating child\n");
-        exit(1);
-      }
-      else if(child == 0)
-      {
-        for(i = 0; i < k; ++i)
-          printf("    ");
-
-        printf("--->%d\n", getpid());
-        //break;
-      }
-      else
-        wait(NULL);
+        printf("Syntax: ./pstree <number of levels> <number of process in each level>\n");
+        return -1;
     }
-  }
+    int lvl = atoi(argv[1]);
+    int proc = atoi(argv[2]);
 
-  return 0;
+    printf("%s %d %d\n", argv[0], lvl, proc);
+
+    for (k = 0; k < lvl; k++)
+    {
+        child = fork();
+
+        if (child < 0)
+        {
+            printf("Error creating child\n");
+            exit(1);
+        }
+        else if (child == 0)
+        {
+            printf("--->%d\n", (int)getpid());
+            for (j = 0; j < proc; j++)
+            {
+                child = fork();
+                if (child < 0)
+                {
+                    printf("Error creating child\n");
+                    exit(1);
+                }
+                else if (child == 0)
+                {
+                    printf("    --->%d\n", (int)getpid());
+                    exit(0);
+                }
+                wait_for_kids();
+            }
+            exit(0);
+        }
+        wait_for_kids();
+    }
+
+    return 0;
 }
-
