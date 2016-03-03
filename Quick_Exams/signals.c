b87b47f771d4403f564effb613b1b58222ca64d1
shiro-saber <shiro.archerl@gmail.com>
2016-03-03 10:11:09 -0600

    El fanstasma de Cubells

---
+++ Quick_Exams/signals.c
@@ -0,0 +1,30 @@
+/* -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.
+* File Name : signals.c
+* Creation Date : 03-03-2016
+* Last Modified : Thu 03 Mar 2016 10:10:54 AM CST
+* Created By : shiro-saber
+
+KNOW LEARN        .==.
+C LANGUAGE       ()''()-.
+      .---.       ;--; /
+    .'_:___". _..'.  __'.
+    |__ --==|'-''' \'...;
+   [  ]  :[|       |---\
+    |__| I=[|     .'    '.
+    / / ____|     :       '._
+   |-/.____.'      | :       :
+  /___\ /___\      '-'._----'
+_._._._._._._._._._._._._._._._._._._._._.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <signal.h>
+
+int main(int argc, char *argv[])
+{
+  
+  
+  return 0;
+}
+
