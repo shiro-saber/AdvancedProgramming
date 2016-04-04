7a4a8d0ed890d72583eb2b8be18381e4bdb1998d
shiro-saber <shiro.archerl@gmail.com>
2016-04-04 10:49:43 -0500

    Homework 4

--- Homework4_Concurrence/dwarfs.c
+++ Homework4_Concurrence/dwarfs.c
@@ -1,7 +1,7 @@
 /* -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.
 * File Name : dwarfs.c
 * Creation Date : 31-03-2016
-* Last Modified : Thu 31 Mar 2016 10:55:48 AM CST
+* Last Modified : Mon 04 Apr 2016 10:49:26 AM CDT
 * Created By : shiro-saber
 
 KNOW LEARN        .==.
@@ -22,86 +22,104 @@ _._._._._._._._._._._._._._._._._._._._._.*/
 #include <sys/types.h>  
 #include <unistd.h>
 #include <time.h>
+#include <semaphore.h>
 
-#define sits 4
-#define dwarfs 7
-
-int taken = 0;
-int dishes = 0;
-
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_mutex_t kuz = PTHREAD_MUTEX_INITIALIZER;
-pthread_cond_t consume_t = PTHREAD_COND_INITIALIZER;
-pthread_cond_t sit = PTHREAD_COND_INITIALIZER;
-pthread_cond_t produce_t = PTHREAD_COND_INITIALIZER;
-
-void *snow(void *arg)
+typedef struct 
 {
-  while(1)
-  {
-    pthread_cond_wait(&produce_t, &mutex);
-    pthread_mutex_lock(&mutex);
-    dishes++;
-    printf("A dwarf want's to eat\n");
-    pthread_mutex_unlock(&mutex);
-    pthread_cond_signal(&consume_t);
-  }
+  int id;
+  pthread_mutex_t serve;
+} dwarf;
 
+int cont;
+sem_t sits, occuped;
+dwarf *table[4];
+
+void *dwarfH(void *arg)
+{
+  dwarf *d = (dwarf*)arg;
+  printf("Dwarf %d going to fap\n", d->id);
+  sleep(rand()%15);
+  printf("Dwarf %d arrived from fapping\n", d->id);
+  int i;
+  sem_wait(&sits);
+  printf("Dwarf %d sit's on table\n", d->id);
+  for(i = 0; i < 4; ++i)
+  {
+    if(table[i] == NULL)
+    {
+      table[i] = d;
+      sem_post(&occuped);
+      pthread_mutex_trylock(&(d->serve));
+      break;
+    }
+  }
+  pthread_mutex_lock(&(d->serve));
+  sleep(2);
+  printf("Dwarf %d finish eating, now it's going for a cigarrete\n", d->id);
+  table[i] = NULL;
+  sem_post(&sits);
   pthread_exit(NULL);
 }
 
-void *dwarf(void *arg)
+void *snowH(void *arg)
 {
-  while(1)
+  int status, cont = 0;
+
+  while(cont < 7)
   {
-    //sleep(1);
-    usleep(rand()%500);
-    pthread_mutex_lock(&kuz);
-    if(taken < sits)
+    int i, j;
+    sem_getvalue(&occuped, &j);
+    if(j > 0)
     {
-      taken++;
-      pthread_cond_signal(&produce_t);
-      printf("I sit on a place, and ask for food\n");
+      for(i = 0; i < 4; ++i)
+      {
+        if(table[i] != NULL)
+        {
+          pthread_mutex_unlock(&(table[i]->serve));
+          printf("I give food and a handjob to dwarf #%d\n", table[i]->id);
+          sem_trywait(&occuped);
+          cont++;
+        }
+      }
     }
     else
     {
-      printf("Waiting for one damn sit!\n");
-      pthread_cond_wait(&sit, &kuz);
-      pthread_cond_signal(&produce_t);
-      printf("I got a chair after wait, and finally i will eat\n");
-      taken++;
+      printf("Snowhite it's going to finger herself\n");
+      sleep(5);
     }
-    
-    pthread_mutex_unlock(&kuz);
-    pthread_cond_wait(&consume_t, &mutex);        
-    pthread_mutex_lock(&mutex);
-    dishes--;
-    taken--;
-    printf("I ate, going to fap\n");
-    pthread_mutex_unlock(&mutex);
-    pthread_cond_signal(&sit);
   }
+  printf("All dwarf's are satisfy\n");
+  pthread_exit(NULL);
 }
+
 int main(int argc, char *argv[])
 {
-  pthread_t *table = (pthread_t*)malloc(sizeof(pthread_t)*dwarfs+1);
-  pthread_t *aux;
-  int index = 0;
+  srand((unsigned)time(NULL));
+  sem_init(&sits, 0, 4);
+  sem_init(&occuped, 0, 0);
+  dwarf *dwarfs = (dwarf*)malloc(sizeof(dwarf)*7);
+  int i = 0;
 
-  for(aux=table; aux < (table+dwarfs); ++aux)
+  for(i; i < 7; ++i)
   {
-    printf("Creating dwarf #%d\n", ++index);
-    pthread_create(aux, NULL, dwarf, (void*)index);
+    (dwarfs+i)->id = i+1;
+    pthread_mutex_init(&(dwarfs+i)->serve, NULL);
   }
 
-  printf("Creating Snowhite\n");
-  ++index;
-  pthread_create(aux, NULL, snow, (void*)index);
+  pthread_t *dwarfp = (pthread_t*)malloc(sizeof(pthread_t)*7);
+  pthread_t *snowp;
+  pthread_create(&snowp, NULL, &snowH, NULL);
 
-  for(aux=table; aux < (table+dwarfs+1); ++aux)
-    pthread_join(*aux, NULL);
+  for(i = 0; i < 7; ++i)
+    pthread_create(dwarfp+i, NULL, &dwarfH, (void*)(dwarfs+i));
 
-  free(table);
+  pthread_join(snowp, NULL);
+  printf("ALLAHUAKBAR!!!!!!\n");
+  sem_destroy(&sits);
+  sem_destroy(&occuped);
+  free(dwarfp);
+  free(dwarfs);
+
   return 0;
 }
 
